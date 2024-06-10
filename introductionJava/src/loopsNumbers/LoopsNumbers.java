965ccb4608672bd435fe6b24c9e44cddb0018c69
danielkremes05 <danielkremes05@gmail.com>
2024-06-10 19:17:15 -0300

    Change Structure Folder

---
+++ introductionJava/src/loopsNumbers/LoopsNumbers.java
@@ -0,0 +1,14 @@
+package loopsNumbers;
+
+public class LoopsNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+
+            int counter = 2;
+
+            System.out.printf("%d x %d: %d \n", counter , i , counter * i);
+        }
+
+    }
+}
